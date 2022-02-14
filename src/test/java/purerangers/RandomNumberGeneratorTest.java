03bb7951cdb47fb7d72839a79e8d8d9470e91ad3
andymurray32 <andrewmurray5@outlook.com>
2022-02-14 13:47:38 +0000

    Test for Random Number
    Checks the probability of values being above 40 and lower than 10 ( 0-50 scale). Failing if the results are abnormally clustered in these regions.

--- src/test/java/purerangers/RandomNumberGeneratorTest.java
+++ src/test/java/purerangers/RandomNumberGeneratorTest.java
@@ -5,19 +5,72 @@ import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static com.purerangers.DateFormat.formatDate;
+import static com.purerangers.RandomNumberGenerator.getRandomNumbersUsingNextInt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomNumberGeneratorTest {
     @Test
-    @DisplayName("Check Distribution of Random number generator")
-    public void checkValidStringToDate() throws ParseException
+    @DisplayName("Check distribution probability above 40")
+    public void checkRandomHigherNumDistribution() throws ParseException
     {
-        String s="21/9/1982";
-        formatDate(s);
-        LocalDate actual= formatDate(s);
-        LocalDate expected = LocalDate.of(1982, 9, 21);
-        assertEquals(expected,actual);
+        /*for mean*/
+        int total = 0;
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0; i<50;i++){
+            int x=getRandomNumbersUsingNextInt();
+            total= total+x;
+            a.add(x);
+        }        // initialization of variables
+        double Z, X, s, u;
+        u = total/50; //mean
+        X=40; // the value to compare distribution
+        double total2 = 0;
+        /*for Standard deviation*/
+        for (int i=0;i<a.size();i++){
+            double b= a.get(i)-u;
+            double v = Math.pow(b,2);
+            total2=total2+v;
+        }
+        double top =total2/50;
+        s=Math.sqrt(top);
+        // master formula
+        Z = (X - u) / s;
+
+       //System.out.println("the Z-value obtained is: " + Z);
+       assertEquals(true,Z<3&&Z>-3);
+    }
+    @Test
+    @DisplayName("Check distribution probability above 40")
+    public void checkRandomLowerNumDistribution() throws ParseException
+    {
+        //NORMAL DISTRIBUTION
+        /*for mean*/
+        int total = 0;
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0; i<50;i++){
+            int x=getRandomNumbersUsingNextInt();
+            total= total+x;
+            a.add(x);
+        }        // initialization of variables
+        double Z, X, s, u;
+        u = total/50; //mean
+        X=10; // the value
+        double total2 = 0;
+        /*for Standard deviation and calculating Z*/
+        for (int i=0;i<a.size();i++){
+            double b= a.get(i)-u;
+            double v = Math.pow(b,2);
+            total2=total2+v;
+        }
+        double top =total2/50;
+        s=Math.sqrt(top);
+        // master formula
+        Z = (X - u) / s;
+
+        //System.out.println("the Z-value obtained is: " + Z);
+        assertEquals(true,Z<3&&Z>-3);
     }
 }
