2fcfec0e61a4f140344985f475fe75a179a8efd9
ThatGuyGilby <jack.m.gilbride@gmail.com>
2022-02-15 14:56:28 +0000

    TimeManager can close TrainingCentres

---
+++ src/test/java/com/purerangers/TimeManagerTests.java
@@ -0,0 +1,76 @@
+package com.purerangers;
+
+import com.purerangers.TrainingCentreTypes.TrainingCentre;
+import com.purerangers.TrainingCentreTypes.TrainingHub;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TimeManagerTests
+{
+    @Test
+    @DisplayName("Can graduate trainees.")
+    public void canBeAddedTo()
+    {
+        TimeManager tm = TimeManager.getInstance();
+        WaitingListHandler wlh = WaitingListHandler.getInstance();
+
+        int numberOfCentres = 10;
+        ArrayList<TrainingCentre> trainingCentres = new ArrayList<>();
+
+        for (int i = 0; i < numberOfCentres; i++)
+        {
+            Queue<Person> trainingQueue = new LinkedList<>();
+
+            for (int j = 0; j < 510; j++)
+            {
+                trainingQueue.add(new Person());
+            }
+
+            wlh.addPeople(trainingQueue);
+
+            TrainingHub hub = new TrainingHub();
+            hub.attemptToRecruitTrainees(wlh.getWaitingList());
+
+            trainingCentres.add(hub);
+        }
+
+        tm.addMonth();
+        tm.addMonth();
+    }
+
+    @Test
+    @DisplayName("Closes centres.")
+    public void centresClose()
+    {
+        TimeManager tm = TimeManager.getInstance();
+        WaitingListHandler wlh = WaitingListHandler.getInstance();
+
+        int numberOfCentres = 10;
+        ArrayList<TrainingCentre> trainingCentres = new ArrayList<>();
+        TrainingCentre.clearCentreList();
+
+        for (int i = 0; i < numberOfCentres; i++)
+        {
+            TrainingHub hub = new TrainingHub();
+
+            trainingCentres.add(hub);
+        }
+
+        int monthsToSimulate = 10;
+
+        for (int i = 0; i < monthsToSimulate; i++)
+        {
+            tm.addMonth();
+        }
+
+        int expected = 0;
+        int actual = TrainingCentre.getOpenCentreList().size();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
