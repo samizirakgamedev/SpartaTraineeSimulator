134e02e98aeb1a3ddee14f235832bac33f467013
mihai <udreamihai@gmail.com>
2022-02-16 11:04:41 +0000

    added course type type to centres

--- src/main/java/com/purerangers/TimeTraveller.java
+++ src/main/java/com/purerangers/TimeTraveller.java
@@ -18,7 +18,7 @@ public class TimeTraveller {
         LocalDate endDate = LocalDate.now().plusMonths(numberOfMonths); //calculate the end of all times
         int variable = 0;
         for (LocalDate date = today; date.isBefore(endDate); date = date.plusMonths(1)) { //loopy loop
-            System.out.println(date + " time to do some sketchy shit, doo daa, doo daa!"); //month by month
+            System.out.println(date + " - time to do some sketchy shit, doo daa, doo daa!"); //month by month
             System.out.println("Today we shall have " + getRandomNumbersUsingNextInt(50, 100) + " poor souls to enlist."); //get the random number of so called trainees
             if (variable % 2 == 0) {
                 String newCentre = GenerateCentreType.generateType(date);
@@ -27,6 +27,7 @@ public class TimeTraveller {
             for (int i = 0; i <= 2; i++) {
                 System.out.println("Available spaces: " + CheckCentresSpaces.returnAvailable(i) + " for " + CheckCentresSpaces.returnCentreType(i));
             }
+            GenerateCentreType.returnCentres();
             System.out.println("=======================");
             variable++;
         }
