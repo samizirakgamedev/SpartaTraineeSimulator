2f01d060232d9f90327572751997cf7d05a9a662
andymurray32 <andrewmurray5@outlook.com>
2022-02-16 12:38:36 +0000

    removed comments for merge with Dev

--- src/main/java/com/purerangers/TimeTraveller.java
+++ src/main/java/com/purerangers/TimeTraveller.java
@@ -33,22 +33,4 @@ public class TimeTraveller {
             variable++;
         }
     }
-    //Centres
-    /* if centre population < 25 for 2 months recurring
-        then close and redestribute people
-    if Centre = training hub
-     population must be <100
-     only 1-3 can be opened
-
-    if centre = bootcamp
-    population <500
-        if it has been open for 3 months then close it
-        only 2 bootcamps can exist
-
-     if tech centre:
-     can only be one course per centre, must be stored randomly
-        */
-//    public static int checkPopulation(int ){
-//
-//    }
 }
